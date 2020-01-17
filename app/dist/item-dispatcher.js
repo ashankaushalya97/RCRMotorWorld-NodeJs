0111ee04fcb1a19a2dd41eafdfb309086e629414
ashankaushalya97 <ashankaushalya952@gmail.com>
2020-01-18 04:51:47 +0530

    Finish item-dispatcher

--- app/dist/item-dispatcher.js
+++ app/dist/item-dispatcher.js
@@ -12,3 +12,60 @@ router.get('', function (req, res) {
         res.send(results);
     });
 });
+router.post('', function (req, res) {
+    if (!(req.body.itemId && req.body.categoryId && req.body.brand && req.body.description && req.body.qtyOnHand
+        && req.body.buyPrice && req.body.unitPrice && typeof req.body.itemId == 'string'
+        && req.body.itemId.trim().length > 0)) {
+        res.sendStatus(400);
+        return;
+    }
+    else {
+        database_1.default.query('INSERT INTO item VALUES (?,?,?,?,?,?,?)', [req.body.itemId, req.body.categoryId, req.body.brand, req.body.description,
+            req.body.qtyOnHand, req.body.buyPrice, req.body.unitPrice], function (err, results) {
+            if (err || results.affectedRows == 0) {
+                // res.send(err);
+                res.sendStatus(500);
+                console.log(err);
+                return;
+            }
+            else {
+                res.sendStatus(201);
+            }
+        });
+    }
+});
+router.put('/:id', function (req, res) {
+    if (!(req.body.itemId && req.body.categoryId && req.body.brand && req.body.description && req.body.qtyOnHand
+        && req.body.buyPrice && req.body.unitPrice && typeof req.body.itemId == 'string'
+        && req.body.itemId.trim().length > 0) && req.body.itemId == req.params.id) {
+        res.sendStatus(400);
+        return;
+    }
+    else {
+        database_1.default.query('UPDATE item SET categoryId=?,brand=?,description=?,qtyOnHand=?,buyPrice=?,unitPrice=? WHERE itemId=?', [req.body.categoryId, req.body.brand, req.body.description,
+            req.body.qtyOnHand, req.body.buyPrice, req.body.unitPrice, req.body.itemId], function (err, results) {
+            if (err || results.affectedRows == 0) {
+                // res.send(err);
+                res.sendStatus(500);
+                console.log(err);
+                return;
+            }
+            else {
+                res.sendStatus(204);
+            }
+        });
+    }
+});
+router.delete('/:id', function (req, res) {
+    database_1.default.query('DELETE FROM item WHERE itemId=?', [req.params.id], function (err, results) {
+        if (err || results.affectedRows == 0) {
+            // res.send(err);
+            res.sendStatus(500);
+            console.log(err);
+            return;
+        }
+        else {
+            res.sendStatus(204);
+        }
+    });
+});
--- app/src/item-dispatcher.ts
+++ app/src/item-dispatcher.ts
@@ -10,3 +10,61 @@ router.get('',(req, res) => {
    })
 });
 
+router.post('',(req, res) => {
+   if(!(req.body.itemId && req.body.categoryId && req.body.brand && req.body.description && req.body.qtyOnHand
+       && req.body.buyPrice && req.body.unitPrice && typeof req.body.itemId == 'string'
+       && req.body.itemId.trim().length>0)){
+      res.sendStatus(400);
+      return;
+   }else{
+      pool.query('INSERT INTO item VALUES (?,?,?,?,?,?,?)',[req.body.itemId,req.body.categoryId,req.body.brand,req.body.description,
+         req.body.qtyOnHand,req.body.buyPrice,req.body.unitPrice],(err, results) => {
+         if(err || results.affectedRows==0){
+            // res.send(err);
+            res.sendStatus(500);
+            console.log(err);
+            return;
+         }else{
+            res.sendStatus(201);
+         }
+
+      })
+
+   }
+});
+
+router.put('/:id',(req, res) => {
+   if(!(req.body.itemId && req.body.categoryId && req.body.brand && req.body.description && req.body.qtyOnHand
+       && req.body.buyPrice && req.body.unitPrice && typeof req.body.itemId == 'string'
+       && req.body.itemId.trim().length>0) && req.body.itemId==req.params.id){
+      res.sendStatus(400);
+      return;
+   }else{
+      pool.query('UPDATE item SET categoryId=?,brand=?,description=?,qtyOnHand=?,buyPrice=?,unitPrice=? WHERE itemId=?',[req.body.categoryId,req.body.brand,req.body.description,
+         req.body.qtyOnHand,req.body.buyPrice,req.body.unitPrice,req.body.itemId],(err, results) => {
+         if(err || results.affectedRows==0){
+            // res.send(err);
+            res.sendStatus(500);
+            console.log(err);
+            return;
+         }else{
+            res.sendStatus(204);
+         }
+
+      })
+
+   }
+});
+
+router.delete('/:id',(req, res) => {
+   pool.query('DELETE FROM item WHERE itemId=?',[req.params.id],(err, results) => {
+      if(err || results.affectedRows==0){
+         // res.send(err);
+         res.sendStatus(500);
+         console.log(err);
+         return;
+      }else{
+         res.sendStatus(204);
+      }
+   })
+});
