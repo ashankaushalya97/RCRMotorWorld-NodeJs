c1eebb37f0131ed28c5c98d59d314187b9070d29
ashankaushalya97 <ashankaushalya952@gmail.com>
2020-01-18 04:34:54 +0530

    Finish delviery-dispatcher

--- app/dist/delivery-dispatcher.js
+++ app/dist/delivery-dispatcher.js
@@ -13,3 +13,50 @@ router.get('', function (req, res) {
         res.send(results);
     });
 });
+router.post('', function (req, res) {
+    if (!(req.body.deliveryId && req.body.orderId && req.body.address && req.body.states && typeof req.body.deliveryId == 'string'
+        && req.body.deliveryId.trim().length > 0)) {
+        res.sendStatus(400);
+        return;
+    }
+    else {
+        database_1.default.query('INSERT INTO delivery VALUES (?,?,?,?)', [req.body.deliveryId, req.body.orderId, req.body.address, req.body.states], function (err, results) {
+            if (err || results.affectedRows == 0) {
+                // res.send(err);
+                res.sendStatus(500);
+                console.log(err);
+                return;
+            }
+            else {
+                res.sendStatus(201);
+            }
+        });
+    }
+});
+router.put('/:id', function (req, res) {
+    if (!(req.body.deliveryId && req.body.orderId && req.body.address && req.body.states && typeof req.body.deliveryId == 'string'
+        && req.body.deliveryId.trim().length > 0 && req.params.id == req.body.deliveryId)) {
+        res.sendStatus(400);
+        return;
+    }
+    database_1.default.query('UPDATE delivery SET orderId=?,address=?,states=? WHERE deliveryId=?', [req.body.orderId, req.body.address, req.body.states, req.body.deliveryId], function (err, results) {
+        if (err || results.affectedRows == 0) {
+            res.send(err);
+            res.sendStatus(500);
+            console.log(err);
+            return;
+        }
+        res.sendStatus(204);
+    });
+});
+router.delete('/:id', function (req, res) {
+    database_1.default.query('DELETE FROM delivery WHERE deliveryId=?', [req.params.id], function (err, results) {
+        if (err || results.affectedRows == 0) {
+            res.send(err);
+            res.sendStatus(500);
+            console.log(err);
+            return;
+        }
+        res.sendStatus(204);
+    });
+});
--- app/src/delivery-dispatcher.ts
+++ app/src/delivery-dispatcher.ts
@@ -1,5 +1,6 @@
 import express = require("express");
 import pool from "./db/database";
+import {type} from "os";
 
 const router = express.Router();
 export default router;
@@ -9,5 +10,58 @@ router.get('',(req, res) => {
       // res.send(results);
       res.send(results);
 
+   })
+});
+router.post('',(req, res) => {
+   if(!(req.body.deliveryId && req.body.orderId && req.body.address && req.body.states && typeof req.body.deliveryId == 'string'
+       && req.body.deliveryId.trim().length>0)){
+      res.sendStatus(400);
+      return;
+   }else{
+      pool.query('INSERT INTO delivery VALUES (?,?,?,?)',[req.body.deliveryId,req.body.orderId,req.body.address,req.body.states],(err, results) => {
+         if(err || results.affectedRows==0){
+            // res.send(err);
+            res.sendStatus(500);
+            console.log(err);
+            return;
+         }else{
+            res.sendStatus(201);
+         }
+
+      })
+
+   }
+
+});
+
+router.put('/:id',(req, res) => {
+   if(!(req.body.deliveryId && req.body.orderId && req.body.address && req.body.states && typeof req.body.deliveryId == 'string'
+       && req.body.deliveryId.trim().length>0 && req.params.id==req.body.deliveryId)){
+      res.sendStatus(400);
+      return;
+   }
+
+   pool.query('UPDATE delivery SET orderId=?,address=?,states=? WHERE deliveryId=?',[req.body.orderId,req.body.address,req.body.states,req.body.deliveryId],(err, results) => {
+      if(err || results.affectedRows==0){
+         res.send(err);
+         res.sendStatus(500);
+         console.log(err);
+         return;
+      }
+      res.sendStatus(204);
+      
+   })
+});
+
+router.delete('/:id',(req, res) => {
+
+   pool.query('DELETE FROM delivery WHERE deliveryId=?',[req.params.id],(err, results) => {
+      if(err || results.affectedRows==0){
+         res.send(err);
+         res.sendStatus(500);
+         console.log(err);
+         return;
+      }
+      res.sendStatus(204);
    })
 });
